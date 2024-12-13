2193895a74a737a922e44adffdc3bc4815a9d34f
roman <roma-t@bk.ru>
2024-12-13 23:36:23 +0300

    lesson1

--- lesson1/mymavenproject/src/main/java/rt/App.java
+++ lesson1/mymavenproject/src/main/java/rt/App.java
@@ -1,15 +1,14 @@
 package rt;
 
 import com.google.common.base.Joiner;
+import java.io.IOException;
+
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) throws IOException {
         String[] words = {"hi", "you"};
         String msg = Joiner.on(", ").join(words);
         System.out.println(msg);
