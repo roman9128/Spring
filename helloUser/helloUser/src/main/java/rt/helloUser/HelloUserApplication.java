62ea2ad7258164b302732a71dfd4d7b7609c17e5
roman <roma-t@bk.ru>
2024-12-18 23:30:02 +0300

    hw2

---
+++ helloUser/helloUser/src/main/java/rt/helloUser/HelloUserApplication.java
@@ -0,0 +1,13 @@
+package rt.helloUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloUserApplication.class, args);
+	}
+
+}
